2c3f3f3449059e2085187855be3d83b600a93838
Timothy Coleman <timhc22@gmail.com>
2022-11-22 13:51:51 +0000

    Add packages and start adding ground

---
+++ src/components/Ground.tsx
@@ -0,0 +1,20 @@
+import * as THREE from "three"
+import { useTexture } from "@react-three/drei"
+import { CuboidCollider, RigidBody } from "@react-three/rapier"
+// import grass from "./assets/grass.jpg"
+const grass = `${process.env.REACT_APP_ASSETS_URL}/grass.jpg`;
+
+
+export function Ground(props: any) {
+  const texture = useTexture(grass)
+  texture.wrapS = texture.wrapT = THREE.RepeatWrapping
+  return (
+    <RigidBody {...props} type="fixed" colliders={false}>
+      <mesh receiveShadow position={[0, 0, 0]} rotation-x={-Math.PI / 2}>
+        <planeGeometry args={[1000, 1000]} />
+        <meshStandardMaterial map={texture} map-repeat={[240, 240]} color="green" />
+      </mesh>
+      <CuboidCollider args={[1000, 2, 1000]} position={[0, -2, 0]} />
+    </RigidBody>
+  )
+}
